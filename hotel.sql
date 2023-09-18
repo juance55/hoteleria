399a08776f969252295ccec9b4838709d3437b1d
juance55 <jrodolfosalazar5@gmail.com>
2023-09-17 23:49:33 -0500

    database

--- hotel.sql
+++ hotel.sql
@@ -1,88 +1,113 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 27-01-2018 a las 00:38:44
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 7.0.13
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- Host: dbhoteleria.mysql.database.azure.com    Database: hotel
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de datos: `hotel`
+-- Table structure for table `contact`
 --
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `contact`
---
-
+DROP TABLE IF EXISTS `contact`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `contact` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `fullname` varchar(100) DEFAULT NULL,
-  `phoneno` int(10) DEFAULT NULL,
+  `phoneno` int DEFAULT NULL,
   `email` text,
   `cdate` date DEFAULT NULL,
-  `approval` varchar(12) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  `approval` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `login`
+-- Dumping data for table `contact`
 --
 
+LOCK TABLES `contact` WRITE;
+/*!40000 ALTER TABLE `contact` DISABLE KEYS */;
+INSERT INTO `contact` VALUES (1,'hola',9856594,'juance@gkail.com','2023-09-18','Not Allowed'),(2,'Juanito',9856547,'juanito@hotmail.com','2023-09-18','Not Allowed');
+/*!40000 ALTER TABLE `contact` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `login` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `usname` varchar(30) DEFAULT NULL,
-  `pass` varchar(30) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `pass` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `login`
+-- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`id`, `usname`, `pass`) VALUES
-(1, 'Admin', '1234'),
-(2, 'Prasath', '12345'),
-(3, 'admin', 'admin');
-
--- --------------------------------------------------------
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (1,'Admin','1234'),(2,'Prasath','12345'),(3,'admin','admin');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `newsletterlog`
+-- Table structure for table `newsletterlog`
 --
 
+DROP TABLE IF EXISTS `newsletterlog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `newsletterlog` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(52) DEFAULT NULL,
   `subject` varchar(100) DEFAULT NULL,
-  `news` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
+  `news` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Estructura de tabla para la tabla `payment`
+-- Dumping data for table `newsletterlog`
 --
 
+LOCK TABLES `newsletterlog` WRITE;
+/*!40000 ALTER TABLE `newsletterlog` DISABLE KEYS */;
+/*!40000 ALTER TABLE `newsletterlog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `payment`
+--
+
+DROP TABLE IF EXISTS `payment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `payment` (
-  `id` int(11) DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(5) DEFAULT NULL,
   `fname` varchar(30) DEFAULT NULL,
   `lname` varchar(30) DEFAULT NULL,
   `troom` varchar(30) DEFAULT NULL,
   `tbed` varchar(30) DEFAULT NULL,
-  `nroom` int(11) DEFAULT NULL,
+  `nroom` int DEFAULT NULL,
   `cin` date DEFAULT NULL,
   `cout` date DEFAULT NULL,
   `ttot` double(8,2) DEFAULT NULL,
@@ -90,60 +115,57 @@ CREATE TABLE `payment` (
   `mepr` double(8,2) DEFAULT NULL,
   `meal` varchar(30) DEFAULT NULL,
   `btot` double(8,2) DEFAULT NULL,
-  `noofdays` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `noofdays` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `payment`
+-- Dumping data for table `payment`
 --
 
-INSERT INTO `payment` (`id`, `title`, `fname`, `lname`, `troom`, `tbed`, `nroom`, `cin`, `cout`, `ttot`, `fintot`, `mepr`, `meal`, `btot`, `noofdays`) VALUES
-(2, 'Dr.', 'Richard ', 'Baily', 'Superior Room', 'Double', 1, '2018-01-05', '2018-01-26', 6720.00, 7123.20, 268.80, 'Breakfast', 134.40, 21),
-(3, 'Mr.', 'Carmen', 'Tajada', 'Deluxe Room', 'Double', 1, '2018-02-01', '2018-02-15', 3080.00, 3264.80, 123.20, 'Breakfast', 61.60, 14);
-
--- --------------------------------------------------------
+LOCK TABLES `payment` WRITE;
+/*!40000 ALTER TABLE `payment` DISABLE KEYS */;
+INSERT INTO `payment` VALUES (2,'Dr.','Richard ','Baily','Superior Room','Double',1,'2018-01-05','2018-01-26',6720.00,7123.20,268.80,'Breakfast',134.40,21),(3,'Mr.','Carmen','Tajada','Deluxe Room','Double',1,'2018-02-01','2018-02-15',3080.00,3264.80,123.20,'Breakfast',61.60,14),(4,'Dr.','Reyner','Baily','Guest House','Single',1,'2018-01-16','2018-01-25',1620.00,1701.00,64.80,'Full Board',16.20,9);
+/*!40000 ALTER TABLE `payment` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `room`
+-- Table structure for table `room`
 --
 
+DROP TABLE IF EXISTS `room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `room` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `type` varchar(15) DEFAULT NULL,
   `bedding` varchar(10) DEFAULT NULL,
   `place` varchar(10) DEFAULT NULL,
-  `cusid` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `cusid` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `room`
+-- Dumping data for table `room`
 --
 
-INSERT INTO `room` (`id`, `type`, `bedding`, `place`, `cusid`) VALUES
-(1, 'Superior Room', 'Single', 'Free', NULL),
-(2, 'Superior Room', 'Double', 'Free', 0),
-(3, 'Superior Room', 'Triple', 'Free', NULL),
-(4, 'Single Room', 'Quad', 'Free', NULL),
-(5, 'Superior Room', 'Quad', 'Free', NULL),
-(6, 'Deluxe Room', 'Single', 'Free', NULL),
-(7, 'Deluxe Room', 'Double', 'NotFree', 3),
-(8, 'Deluxe Room', 'Triple', 'Free', NULL),
-(9, 'Deluxe Room', 'Quad', 'Free', NULL),
-(10, 'Guest House', 'Single', 'Free', NULL),
-(11, 'Guest House', 'Double', 'Free', NULL),
-(12, 'Guest House', 'Quad', 'Free', NULL),
-(13, 'Single Room', 'Single', 'Free', NULL),
-(14, 'Single Room', 'Double', 'Free', NULL),
-(15, 'Single Room', 'Triple', 'Free', NULL);
-
--- --------------------------------------------------------
+LOCK TABLES `room` WRITE;
+/*!40000 ALTER TABLE `room` DISABLE KEYS */;
+INSERT INTO `room` VALUES (1,'Superior Room','Single','Free',NULL),(2,'Superior Room','Double','Free',0),(3,'Superior Room','Triple','Free',NULL),(4,'Single Room','Quad','Free',NULL),(5,'Superior Room','Quad','Free',NULL),(6,'Deluxe Room','Single','Free',NULL),(7,'Deluxe Room','Double','NotFree',3),(8,'Deluxe Room','Triple','Free',NULL),(9,'Deluxe Room','Quad','Free',NULL),(10,'Guest House','Single','NotFree',4),(11,'Guest House','Double','Free',NULL),(12,'Guest House','Quad','Free',NULL),(13,'Single Room','Single','Free',NULL),(14,'Single Room','Double','Free',NULL),(15,'Single Room','Triple','Free',NULL);
+/*!40000 ALTER TABLE `room` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Estructura de tabla para la tabla `roombook`
+-- Table structure for table `roombook`
 --
 
+DROP TABLE IF EXISTS `roombook`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roombook` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `Title` varchar(5) DEFAULT NULL,
   `FName` text,
   `LName` text,
@@ -158,80 +180,28 @@ CREATE TABLE `roombook` (
   `cin` date DEFAULT NULL,
   `cout` date DEFAULT NULL,
   `stat` varchar(15) DEFAULT NULL,
-  `nodays` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `nodays` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Volcado de datos para la tabla `roombook`
+-- Dumping data for table `roombook`
 --
 
-INSERT INTO `roombook` (`id`, `Title`, `FName`, `LName`, `Email`, `National`, `Country`, `Phone`, `TRoom`, `Bed`, `NRoom`, `Meal`, `cin`, `cout`, `stat`, `nodays`) VALUES
-(3, 'Mr.', 'Carmen', 'Tajada', 'carmen@gmail.com', 'Sri Lankan', 'Peru', '923122323', 'Deluxe Room', 'Double', '1', 'Breakfast', '2018-02-01', '2018-02-15', 'Conform', 14),
-(4, 'Dr.', 'Reyner', 'Baily', 'ricardocorazondeleon2018@gmail.com', 'Sri Lankan', 'Costa Rica', '923342343', 'Guest House', 'Single', '1', 'Full Board', '2018-01-16', '2018-01-25', 'Not Conform', 9);
+LOCK TABLES `roombook` WRITE;
+/*!40000 ALTER TABLE `roombook` DISABLE KEYS */;
+INSERT INTO `roombook` VALUES (3,'Mr.','Carmen','Tajada','carmen@gmail.com','Sri Lankan','Peru','923122323','Deluxe Room','Double','1','Breakfast','2018-02-01','2018-02-15','Conform',14),(4,'Dr.','Reyner','Baily','ricardocorazondeleon2018@gmail.com','Sri Lankan','Costa Rica','923342343','Guest House','Single','1','Full Board','2018-01-16','2018-01-25','Conform',9);
+/*!40000 ALTER TABLE `roombook` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `contact`
---
-ALTER TABLE `contact`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `login`
---
-ALTER TABLE `login`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `newsletterlog`
---
-ALTER TABLE `newsletterlog`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `room`
---
-ALTER TABLE `room`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `roombook`
---
-ALTER TABLE `roombook`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `contact`
---
-ALTER TABLE `contact`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `login`
---
-ALTER TABLE `login`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT de la tabla `newsletterlog`
---
-ALTER TABLE `newsletterlog`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT de la tabla `room`
---
-ALTER TABLE `room`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
---
--- AUTO_INCREMENT de la tabla `roombook`
---
-ALTER TABLE `roombook`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-17 23:47:32
