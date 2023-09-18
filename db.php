3b3ca9f2a88a4a9f250a340ff382151a689bb5f4
juance55 <jrodolfosalazar5@gmail.com>
2023-09-17 23:41:30 -0500

    update 2

--- db.php
+++ db.php
@@ -1,4 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","hotel") or die(mysql_error());
-
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_real_connect($con, "dbhoteleria.mysql.database.azure.com", "Administrador", "Autenticacion1234", "hotel", 3306, MYSQLI_CLIENT_SSL);
 ?>
