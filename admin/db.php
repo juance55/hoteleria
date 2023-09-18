2485efb494516375366d4933848165322d07248b
juance55 <jrodolfosalazar5@gmail.com>
2023-09-17 23:42:51 -0500

    update 2

--- admin/db.php
+++ admin/db.php
@@ -1,5 +1,5 @@
 <?php
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "../ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "dbhoteleria.mysql.database.azure.com", "Administrador", "Autenticacion1234", "hotel", 3306, MYSQLI_CLIENT_SSL);
 ?>
