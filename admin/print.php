f2899bdaa600afbb7ccaa30a306804f9a050bde7
Rodolfo Cedeño Salazar <jrodolfosalazar5@gmail.com>
2023-09-29 10:54:09 -0500

    conexion

--- admin/print.php
+++ admin/print.php
@@ -255,9 +255,9 @@ tr:hover .cut { opacity: 1; }
 		<header>
 			<h1>Invoice</h1>
 			<address >
-				<p>HOTEL AMANECER,</p>
-				<p>Nueva carretera de Kalmunai,<br>Batusai,<br>Sri Lanka.</p>
-				<p>(+94) 65 222 44 55</p>
+				<p>Hoteles Resort,</p>
+				<p>Ecuador,<br>Manabí,<br>Ecuador.</p>
+				<p>(+593) 0985651144 </p>
 			</address>
 			<span><img alt="" src="assets/img/sun.png"></span>
 		</header>
@@ -331,7 +331,7 @@ tr:hover .cut { opacity: 1; }
 		<aside>
 			<h1><span >Contáctenos</span></h1>
 			<div >
-				<p align="center">Email :- tusolutionweb@sgmail.com || Web :- http://tusolutionweb.blogspot.pe/ || Phone :- +94 65 222 44 55 </p>
+				<p align="center">Email :- hotel@admin.com || Web :- https://hotelit.azurewebsites.net// || Phone :- +593 0985651144 </p>
 			</div>
 		</aside>
 	</body>
